657bfa9d7877682306e035e47531c7ddd124a7cf
Ex DB2OO <60365153+exDB2OO@users.noreply.github.com>
2020-01-31 16:40:37 +0100

    Added ADF4351 and changed pin assignments for LCD1602

--- example/adf4350-advanced/adf4350-advanced.ino
+++ example/adf4350-advanced/adf4350-advanced.ino
@@ -1,12 +1,22 @@
 #include <SPI.h>
-#include <ADF4350.h>
+#include "ADF4350.h"
+
+#if defined(ESP8266)
+#define PLL_LE_PIN D8
+#else
 #define PLL_LE_PIN 8
+#endif
+
+#define SCAN_I2C
+
 ADF4350 PLL(PLL_LE_PIN);
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 //i2c address and pin config for pcf8574 board, maybe differ
-LiquidCrystal_I2C lcd(0x20, 4, 5, 6, 0, 1, 2, 3, 7, NEGATIVE);
+// DB2OO changed
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE);
+// LiquidCrystal_I2C lcd(0x20, 4, 5, 6, 0, 1, 2, 3, 7, NEGATIVE);
 
 #include <Encoder.h>
 Encoder encoder(3, 2); //pins with interrupt support prefer
@@ -14,21 +24,44 @@ long oldPosition  = 0;
 #define ENCODER_SW_PIN 5
 #define ENCODER_CLICK_STEP 4.0 //steps between fixed position of encoder
 
+#if defined(ESP8266)
+#define LOCK_PIN D5 //lock status from ADF4350 via MUXOUT pin
+#else
 #define LOCK_PIN 4 //lock status from ADF4350 via MUXOUT pin
-#define HEAT_PIN 6 //heat status from FE-5680A rubidium frequency standard
+#endif
+
+// DB2OO, 29.12.19: don't define HEAT_PIN, as I don't have one ;-)
+//#define HEAT_PIN 6 //heat status from FE-5680A rubidium frequency standard
 bool pll_lock = false;
 bool rubid_heating = false;
 
+#if defined(ESP8266)
+#define eeprom_read_dword(x) (0)
+#define eeprom_read_byte(x) (0)
+#define eeprom_write_dword(x,y) (0)
+#define eeprom_write_byte(x,y) (0)
+#else
+
 #include <avr/eeprom.h>
-#define EEPROM_WRITE_TIMEOUT 60000UL //save current config to EEPROM after 60 sec of last change
+
+#endif
+#define EEPROM_WRITE_TIMEOUT 10000UL //save current config to EEPROM after 10 sec of last change, DB2OO
 #define EEPROM_ADDR_FREQ 0
 #define EEPROM_ADDR_CURPOS 5
 #define EEPROM_ADDR_POWER 6
 unsigned long eeprom_write_timeout = 0;
 
+#ifdef _ADF4351
+//DB2OO: ADF4351 support
+#define PLL_REFERENCE_FREQ 25 //MHz clock
+// DB2OO: My ADF4351 supports min. Freq  of 32.4 MHz
+#define PLL_FREQ_MIN 32000 //kHz
+#define PLL_FREQ_MAX 4400000 //kHz
+#else
 #define PLL_REFERENCE_FREQ 10 //MHz clock
 #define PLL_FREQ_MIN 137000 //kHz
 #define PLL_FREQ_MAX 4400000 //kHz
+#endif
 
 #define PLL_POWER_MIN 0
 #define PLL_POWER_MAX 3
@@ -45,15 +78,31 @@ int8_t pll_rf_power = 0;
 
 void setup()
 {
+    int i;
+  delay(1000);
+  // ESP8266 has an issue with the Serial interface: need to start with another baud rate
+#ifdef ESP8266
+  Serial.begin(9600);
+  while (! Serial) delay(1);
+#endif
   Serial.begin(115200);
+#ifdef ESP8266
+  while (! Serial) delay(1);
+#endif
 
+  Wire.begin();
+#ifdef SCAN_I2C
+  Serial.println("\nI2C Scanner");
+#endif  
   lcd.begin(16, 2);
   lcd.backlight();
   lcd.home();
   lcd.print("Hello, world!");
 
   //spike for ADF4350 cold start
-  delay(5000);
+  for (i=0; i<1; i++) delay(5);
+
+#ifndef SCAN_I2C
 
   freq = eeprom_read_dword(EEPROM_ADDR_FREQ);
   if (freq >= PLL_FREQ_MIN && freq <= PLL_FREQ_MAX) {
@@ -71,13 +120,22 @@ void setup()
   PLL.initialize(freq, PLL_REFERENCE_FREQ);
   PLL.setRfPower(pll_rf_power);
 
+#ifdef HEAT_PIN
   pinMode(HEAT_PIN, INPUT);
+#endif
   pinMode(LOCK_PIN, INPUT);
   pinMode(ENCODER_SW_PIN, INPUT);
   digitalWrite(ENCODER_SW_PIN, HIGH);
 
+#endif  // SCAN__I2C
+
   printLCD();
   lcd.cursor();
+  
+#ifndef SCAN__I2C
+  //DB2OO: set initial freq.
+  PLL.setFreq(freq);
+#endif  // SCAN__I2C
 }
 
 void printLCD()
@@ -92,6 +150,10 @@ void printLCD()
   lcd.setCursor(0, 0);
   lcd.print("F =");
   if (part1 < 1000)lcd.print(" ");
+  // DB2OO
+  if (part1 < 100)lcd.print(" ");
+  // DB2OO
+  if (part1 < 10)lcd.print(" ");
   lcd.print(part1);
   lcd.print(".");
   if (part2 < 100)lcd.print("0");
@@ -110,7 +172,8 @@ void printLCD()
       lcd.print("-1");
       break;
     case 2:
-      lcd.print(" 3");
+      //DB2OO: 2 dBm, not 3
+      lcd.print(" 2");
       break;
     case 3:
       lcd.print(" 5");
@@ -140,11 +203,49 @@ void printLCD()
 
 void loop()
 {
+#ifdef SCAN_I2C
+  // I2C Scanner
+  byte error, address;
+  int nDevices;
+  int i;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  for (i=0; i<1; i++) { yield(); delay(5);}
+#else
+
+  // DB2OO, 29.12.19: check Heat pin only, if defined
+#ifdef HEAT_PIN
   if(rubid_heating != digitalRead(HEAT_PIN)){
     rubid_heating = !rubid_heating;
     PLL.update(); //for sure about that
     printLCD();
   }
+#endif
   
   if (digitalRead(LOCK_PIN) != pll_lock) {
     pll_lock = !pll_lock;
@@ -232,6 +333,5 @@ void loop()
     eeprom_write_byte(EEPROM_ADDR_CURPOS, cursor_pos);
     eeprom_write_byte(EEPROM_ADDR_POWER, pll_rf_power);
   }
-
+#endif  // SCAN_I2C
 }
-
